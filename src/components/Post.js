cdd3446e6c1c0ca5f954916252f9b6a33c6dbe12
HenriqueSilva <HenriqueSilva254@gmail.com>
2023-08-17 19:28:42 -0300

    like falta recarregar o post curtido

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,126 +1,181 @@
 import React, { useState } from "react";
 import { styled } from "styled-components";
-import { likeIcon, likedIcon, editIcon, deleteIcon,} from "../images/IconsIndex";
+import {
+  likeIcon,
+  likedIcon,
+  editIcon,
+  deleteIcon,
+} from "../images/IconsIndex";
 import postservices from "../services/post.service";
 import { useContext } from "react";
 import { UserContext } from "../Context/Context";
+import { Tooltip as ReactTooltip } from "react-tooltip";
 
+export default function Post({ post }) {
+  const { user } = useContext(UserContext);
+  const [iconelike, setIconelike] = useState(0);
 
-
-
-export default function Post({ post }){
-    const {user} = useContext(UserContext)
-    const [iconelike, setIconelike] = useState(0);
-    console.log(post)
-
-    function formatNames(names){
-      const maxShown = 2;
+  function formatNames(names) {
     
-      const shownNames = names.slice(-maxShown).join(', ')
-      if (names.length > 2) {
-        const remainingCount = names.length - 2;
-        return `Curtido por ${shownNames} e mais ${remainingCount} pessoa${remainingCount > 1 ? 's' : ''}`;
-      }
-      return `Curtido por ${shownNames}`;
+    const maxShown = 2;
+    const shownNames = names.slice(-maxShown).join(", ");
+    if (names.length > 2) {
+      const remainingCount = names.length - 2;
+      return `Curtido por ${shownNames} e mais ${remainingCount} pessoa${
+        remainingCount > 1 ? "s" : ""
+      }`;
     }
-    
-  
-  
-    
-    function darLike(body) {
-      const likes = iconelike === 0 ? 1 : 0
-      console.log(user)
-      //falta colocar no body o id do usuario que esta logado
-      if(likes === 1){
-          console.log('post')
-          //postservices.likepost(body)
-      }else{
-          console.log('dellete')
-          //postservices.deslike(body)
-      }
-  
-  
-      setIconelike(likes)
+    console.log(names)
+    if(names[0] === null) return '';
+    return `Curtido por ${names}`;
+  }
+
+  function darLike(postId) {
+    const likes = iconelike === 0 ? 1 : 0;
+    const body ={
+        postId,
+        userliked: user.id
     }
-    
-    return (
-        <PostDiv>
-            <LeftContent>
-                <img src={post.userPhoto} alt={`Foto de ${post.userPhoto}`} />
-                 <Likes>
-          <div onClick={()=> darLike(post.postId)}>
-          <Icon src={iconelike === 0 ? likeIcon : likedIcon} alt="Curtir" 
-          title={formatNames(post.usersLikedNames)}/>
-          <p>{post.usersLikedNames.length + iconelike}</p>
-          </div>
-        </Likes> 
-            </LeftContent>
-            <MainContent>
-                <div>
-                    <h5>{post.userName}</h5>
-                    <IconsDiv>
-                        <Icon src={editIcon} alt='Editar' />
-                        <Icon src={deleteIcon} alt='Deletar' />
-                    </IconsDiv>
-                </div>
-                <p>{post.description}</p>
-                 <Metadados onClick={() => {
-                    window.open(post.url, '_blank')
-                }}>
-                    <MetadadosText>
-                        <p>{post.metadata['og:title']}</p>
-                        <p>{post.metadata['og:description']}</p>
-                        <p>{post.url}</p>
-                    </MetadadosText>
-                    <img src={post.metadata['og:image']} alt='URL Preview' />
-                </Metadados> 
-            </MainContent>
-        </PostDiv>
-    );
-};
+    //falta colocar no body o id do usuario que esta logado
+    if (likes === 1) {
+      postservices.likepost(body)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data))
+    } else {
+      postservices.deslike(body)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data))
+    }
+
+    setIconelike(likes);
+  }
+
+  return (
+    <PostDiv>
+      <LeftContent>
+        <img src={post.userPhoto} alt={`Foto de ${post.userPhoto}`} />
+
+        <div>
+          {post.usersLikedNames ? (
+            <Likes>
+              <div onClick={() => darLike(post.postId, user.id)}>
+                {post.usersLikedNames.includes(user.name)? 
+                (
+                  <Icon
+                  src={likedIcon}
+                  alt="Curtir"
+                  data-tooltip-id={post.postId}
+                />
+                ):
+                ( <Icon
+                  src={likeIcon}
+                  alt="Curtir"
+                  data-tooltip-id={post.postId}
+                />)}
+               
+                <ReactTooltip 
+                id={post.postId} 
+                place="bottom" 
+                content={formatNames(post.usersLikedNames)}
+                style={{
+                  width: '  ',
+                  backgroundColor: '#FFFFFFE5', // Cor de fundo
+                  color: 'black', // Cor do texto
+                  padding: '8px', // Espaçamento interno
+                  borderRadius: '4px', // Borda arredondada
+                  fontSize: '12px', // Tamanho da fonte
+                }}
+                />
+                {post.usersLikedNames ? (<p>{post.usersLikedNames.length}</p>):(<p>0</p>)}
+               
+              </div>
+            </Likes>
+          ) : (
+            <Likes>
+              <div onClick={() => darLike(post.postId)}>
+                <Icon
+                  src={iconelike === 0 ? likeIcon : likedIcon}
+                  alt="Curtir"
+                />
+              </div>
+            </Likes>
+          )}
+        </div>
+      </LeftContent>
+      <MainContent>
+        <div>
+          <h5>{post.userName}</h5>
+          <IconsDiv>
+            <Icon src={editIcon} alt="Editar" />
+            <Icon src={deleteIcon} alt="Deletar" />
+          </IconsDiv>
+        </div>
+        <p>{post.description}</p>
+        <div>
+          {post.Metadados ? (
+            <Metadados
+              onClick={() => {
+                window.open(post.url, "_blank");
+              }}
+            >
+              <MetadadosText>
+                <p>{post.metadata["og:title"]}</p>
+                <p>{post.metadata["og:description"]}</p>
+                <p>{post.url}</p>
+              </MetadadosText>
+              <img src={post.metadata["og:image"]} alt="URL Preview" />
+            </Metadados>
+          ) : (
+            ""
+          )}
+        </div>
+      </MainContent>
+    </PostDiv>
+  );
+}
 const Likes = styled.div``;
 const IconsDiv = styled.div``;
 const Icon = styled.img``;
 const LeftContent = styled.div``;
 const MainContent = styled.div``;
-const MetadadosText = styled.div``
+const MetadadosText = styled.div``;
 const Metadados = styled.div`
-    cursor: pointer;
-    margin-top: 0.5em;
-    width: 100%;
+  cursor: pointer;
+  margin-top: 0.5em;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  border: 1px solid #4d4d4d;
+  border-radius: 0.7em;
+
+  ${MetadadosText} {
+    width: calc(100% - 10em);
     display: flex;
-    flex-direction: row;
-    border: 1px solid #4D4D4D;
-    border-radius: 0.7em;
+    flex-direction: column;
+    gap: 1em;
+    padding: 1em;
 
-    ${MetadadosText}{
-        width: calc(100% - 10em);
-        display: flex;
-        flex-direction: column;
-        gap: 1em;
-        padding: 1em;
-
-        p:nth-child(1) {
-            color: #CECECE;
-            font-size: 16px;
-        }
-        p:nth-child(2) {
-            color: #9B9595;
-            font-size: 11px;
-        }
-        p:nth-child(3) {
-            color: #CECECE;
-            font-size: 11px;
-        }
-    }                   
-
-    img{
-        aspect-ratio: 1;
-        object-fit: cover;
-        max-width: 10em;
-        border-radius: 0em 0.7em 0.7em 0em;
+    p:nth-child(1) {
+      color: #cecece;
+      font-size: 16px;
     }
-`
+    p:nth-child(2) {
+      color: #9b9595;
+      font-size: 11px;
+    }
+    p:nth-child(3) {
+      color: #cecece;
+      font-size: 11px;
+    }
+  }
+
+  img {
+    aspect-ratio: 1;
+    object-fit: cover;
+    max-width: 10em;
+    border-radius: 0em 0.7em 0.7em 0em;
+  }
+`;
 const PostDiv = styled.div`
   display: flex;
   padding: 1em;
@@ -129,62 +184,66 @@ const PostDiv = styled.div`
   background-color: #171717;
 
   ${LeftContent}
-    width: 3.5em;
+  width: 3.5em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5em;
+  background-color: #171717;
+  max-width: 38em;
+
+  ${LeftContent} {
+    max-width: 4.5em;
     display: flex;
     flex-direction: column;
     align-items: center;
     gap: 0.5em;
-    background-color: #171717;
-    max-width: 38em;
 
-    ${LeftContent}{
-        max-width: 4.5em;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 0.5em;
-
-        img{
-            width: 100%;
-            aspect-ratio: 1;
-            object-fit: cover;
-            border-radius: 3em;
-        }
-        ${Icon}{
-            padding-top: 0.6em;
-            width: 50%;
-            aspect-ratio: auto;
-            border-radius: 0em;
-            cursor: pointer;
-        }
-        p{
-            font-size: 12px;
-        }
+    img {
+      width: 100%;
+      aspect-ratio: 1;
+      object-fit: cover;
+      border-radius: 3em;
     }
-    ${MainContent}{
-        max-width: 30em;
-        display: flex;
-        flex-direction: column;
-        gap: 0.5em;
-        div{
-            display: flex;
-            justify-content: space-between;
-            ${IconsDiv}{
-                width: 2.75em;
-                ${Icon}{
-                    cursor: pointer;
-                    @media screen and (max-width: 768px) {width: 1.75em;}
-                }
-
-                @media screen and (max-width: 768px) {width: 4em;}
-            }
-        }
-        p{
-            color: #B7B7B7;
-        }
+    ${Icon} {
+      padding-top: 0.6em;
+      width: 50%;
+      aspect-ratio: auto;
+      border-radius: 0em;
+      cursor: pointer;
     }
-
-    @media screen and (max-width: 768px) {
-        max-width: 100vw;
+    p {
+      font-size: 12px;
     }
-`
+  }
+  ${MainContent} {
+    max-width: 30em;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5em;
+    div {
+      display: flex;
+      justify-content: space-between;
+      ${IconsDiv} {
+        width: 2.75em;
+        ${Icon} {
+          cursor: pointer;
+          @media screen and (max-width: 768px) {
+            width: 1.75em;
+          }
+        }
+
+        @media screen and (max-width: 768px) {
+          width: 4em;
+        }
+      }
+    }
+    p {
+      color: #b7b7b7;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    max-width: 100vw;
+  }
+`;
--- src/pages/PublishPage.js
+++ src/pages/PublishPage.js
@@ -7,6 +7,28 @@ import Post from '../components/Post';
 export default function PublishPage() {
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [link, setLink] = useState('');
+    const [description, setDescription] = useState('');
+    const [isDisabled, setIsDisabled] = useState(false);
+
+    const handlePublish = () => {
+        if (!link) return alert("Please enter a link before publishing!");
+
+        setIsDisabled(true);
+
+        axios.post(`${process.env.REACT_APP_API_URL}/posts`, { link, description })
+            .then(resp => {
+                setPosts([resp.data, ...posts]);
+
+                setLink('');
+                setDescription('');
+                setIsDisabled(false);
+            })
+            .catch(error => {
+                alert("An error occurred while trying to publish the post");
+                setIsDisabled(false);
+            });
+    };
 
     useEffect(()=>{
         axios.get(`${process.env.REACT_APP_API_URI}/posts`)
@@ -23,13 +45,27 @@ export default function PublishPage() {
 
 
     return (
-        <>
+        <PageContainer>
             <header>Linkr</header>
             <h1>Timeline</h1>
             <div>
                 <h3>What are you going to share today?</h3>
-                <input type="text" label="Link" required></input>
-                <input type="text" label="Descrição"></input>
+                <input 
+                    type="text" 
+                    label="Link" 
+                    onChange={e => setLink(e.target.value)} 
+                    disabled={isDisabled}
+                    placeholder="http://..."
+                    required 
+                ></input>
+                <input 
+                    type="text" 
+                    label="Descrição" 
+                    onChange={e => setDescription(e.target.value)}
+                    disabled={isDisabled}
+                    placeholder="Awesome article about #javascript"
+                ></input>
+                <button onClick={handlePublish}>Publish</button>
                 <button>Publish</button>
             </div>
 
@@ -53,12 +89,28 @@ export default function PublishPage() {
                 </>
             )}
             </PostsList>
-        </>
+        </PageContainer>
     )
 }
+const PageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+    padding-top: 1em;
+    margin: auto;
+    gap: 2em;
+    min-height: 100vh;
+    max-width: 100vw;
+`;
 const PostsList = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: center;
     gap: 1em;
     padding-top: 2em;
+
+    @media screen and (max-width: 768px) {
+        width: 100vw;
+    }
 `
